e6cc7799059b767eaa1fae7cfe3d3109f33ff14a
a12345 <bubenko1488@gmail.com>
2025-02-01 02:00:33 +0200

    init

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import './styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Импорт стилей Bootstrap
+import App from './App.jsx'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+
+createRoot(document.getElementById('root')).render(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/:orderId" element={<App />} />
+            <Route path="*" element={<div>Invalid URL</div>} />
+        </Routes>
+    </BrowserRouter>,
+)
